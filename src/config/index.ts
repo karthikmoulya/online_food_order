cf4a60edfc2092cce7420b10d7699f92fdb55248
karthikmoulya <karthikmoulya1995@gmail.com>
2023-09-01 18:24:04 +0530

    minor fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const MONGO_URI = process.env.MONGO_URI;
 
-export const APP_SECRET = 'Our_App_Secret';
+export const APP_SECRET = process.env.APP_SECRET;
 
 export const PORT = process.env.PORT || 8000;
